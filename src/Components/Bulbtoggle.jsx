8448e1876616031e3773a135a982b89cfb932878
Umar Habib <you@example.comumarhabib464@gmail.com>
2022-02-12 21:35:53 +0500

    Bulbtoggle

---
+++ src/Components/Bulbtoggle.jsx
@@ -0,0 +1,27 @@
+import React , {useState} from 'react'
+import bulbon from  '../images/bulb-on.png'
+import bulboff from  '../images/bulb-off.png'
+import "./style.css"
+
+function Bulbtoggle() {
+    
+    const [bulbState,bulbStateChange] = useState(true)
+    
+  let theSource = bulbon
+    const clickButton = () => {
+      
+        bulbStateChange(!bulbState)
+
+        }
+        return (
+          <div className='bulbBox'>
+            <img src={bulbState === true ? bulbon : bulboff} alt="" width={400}/>
+            <button className="btn btn-dark btn-lg" onClick={clickButton}>Click Me!</button>
+          </div>
+        )
+    }
+
+  
+  
+
+export default Bulbtoggle
